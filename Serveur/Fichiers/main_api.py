56cac0d4e885825d5de3cace067023faae66cd79
Julien <julien.maeckelberg@ipsa.fr>
2026-01-10 22:06:52 +0100

    Update main_api.py

--- Serveur/Fichiers/main_api.py
+++ Serveur/Fichiers/main_api.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, File, UploadFile
 from datetime import datetime
 from PIL import Image
+import noseConeShape
 import io
 import os
 
@@ -60,57 +61,51 @@ async def predict(file: UploadFile = File(...)):
     # masque_vert
     ) = analyse_image(image)
 
-    Donnes = {}
+    Donnees = {}
 
-    Donnes["Long_ogive"] = int(hauteur_jaune) # Hauteur de l'ogive
-    Donnes["D_og"] = int(epaisseur_moyenne_rouge) # Diamètre à la base de l'ogive
-    Donnes["D_ref"] = int(Donnes["D_og"]) # Égal au diamètre de l'ogive
-    Donnes["Nb_trans"] = int(0) # Nombre de transitions
+    Donnees["Long_ogive"] = int(hauteur_jaune) # Hauteur de l'ogive
+    Donnees["D_og"] = int(epaisseur_moyenne_rouge) # Diamètre à la base de l'ogive
+    Donnees["D_ref"] = int(Donnees["D_og"]) # Égal au diamètre de l'ogive
+    Donnees["Nb_trans"] = int(0) # Nombre de transitions
 
     # Ailerons du bas
-    Donnes["m_ail"] = int(hauteur_droite_verte) # Emplanture m
-    Donnes["n_ail"] = int(hauteur_gauche_verte) # Saumon n
-    Donnes["p_ail"] = int(fleche) # Flèche p
-    Donnes["E_ail"] = int(envergure) # Envergure E
-    Donnes["X_ail"] = int(hauteur_rouge + hauteur_jaune) # Position du bas
-    Donnes["D_ail"] = int(epaisseur_moyenne_rouge) # Diamètre aux ailerons
+    Donnees["m_ail"] = int(hauteur_droite_verte) # Emplanture m
+    Donnees["n_ail"] = int(hauteur_gauche_verte) # Saumon n
+    Donnees["p_ail"] = int(fleche) # Flèche p
+    Donnees["E_ail"] = int(envergure) # Envergure E
+    Donnees["X_ail"] = int(hauteur_rouge + hauteur_jaune) # Position du bas
+    Donnees["D_ail"] = int(epaisseur_moyenne_rouge) # Diamètre aux ailerons
 
     # Ailerons du haut (canard)
-    Donnes["m_can"] = int(60) # Emplanture m
-    Donnes["n_can"] = int(25) # Saumon n
-    Donnes["p_can"] = int(20) # Flèche p
-    Donnes["E_can"] = int(45) # Envergure E
-    Donnes["X_can"] = int(500) # Position du bas
-    Donnes["D_can"] = int(54) # Diamètre aux ailerons
+    Donnees["m_can"] = int(60) # Emplanture m
+    Donnees["n_can"] = int(25) # Saumon n
+    Donnees["p_can"] = int(20) # Flèche p
+    Donnees["E_can"] = int(45) # Envergure E
+    Donnees["X_can"] = int(500) # Position du bas
+    Donnees["D_can"] = int(54) # Diamètre aux ailerons
 
-    Donnes["Long_tot"] = int(hauteur_rouge + hauteur_jaune) # Longueur totale
+    Donnees["Long_tot"] = int(hauteur_rouge + hauteur_jaune) # Longueur totale
 
     # Transition 1
-    Donnes["l_j"] = int(30) # Longueur de la transition
-    Donnes["D1j"] = Donnes["D_og"] # Diamètre en haut de la transition
-    Donnes["D2j"] = int(50) # Diamètre en bas de la transition
-    Donnes["X_j"] = int(300) # Position depuis le sommet de l'ogive
+    Donnees["l_j"] = int(30) # Longueur de la transition
+    Donnees["D1j"] = Donnees["D_og"] # Diamètre en haut de la transition
+    Donnees["D2j"] = int(50) # Diamètre en bas de la transition
+    Donnees["X_j"] = int(300) # Position depuis le sommet de l'ogive
 
     # Transition 2
-    Donnes["l_r"] = int(27) # Longueur de la transition
-    Donnes["D1r"] = int(Donnes["D2j"]) # Diamètre en haut de la transition
-    Donnes["D2r"] = int(54) # Diamètre en bas de la transition
-    Donnes["X_r"] = int(360) # Position depuis le sommet de l'ogive
+    Donnees["l_r"] = int(27) # Longueur de la transition
+    Donnees["D1r"] = int(Donnees["D2j"]) # Diamètre en haut de la transition
+    Donnees["D2r"] = int(54) # Diamètre en bas de la transition
+    Donnees["X_r"] = int(360) # Position depuis le sommet de l'ogive
 
     # Transition 3
-    Donnes["l_s"] = int(10) # Longueur de la transition
-    Donnes["D1s"] = int(Donnes["D2r"]) # Diamètre en haut de la transition
-    Donnes["D2s"] = int(10) # Diamètre en bas de la transition
-    Donnes["X_s"] = int(10) # Position depuis le sommet de l'ogive
+    Donnees["l_s"] = int(10) # Longueur de la transition
+    Donnees["D1s"] = int(Donnees["D2r"]) # Diamètre en haut de la transition
+    Donnees["D2s"] = int(10) # Diamètre en bas de la transition
+    Donnees["X_s"] = int(10) # Position depuis le sommet de l'ogive
 
-    Donnes["XpropuRef"] = Donnes["Long_tot"]
+    Donnees["XpropuRef"] = Donnees["Long_tot"]
 
-    img_ogive = masque_jaune # Ogive segmentée pour Pablo
+    Donnees["Forme_ogive"] = noseConeShape.noseConeType(masque_jaune)
 
-    img_ogive_list = img_ogive.tolist()
-
-    return {
-        "donnees": Donnes,
-        "img_ogive": img_ogive_list,
-        "img_ogive_shape": img_ogive.shape
-    }
+    return Donnees
